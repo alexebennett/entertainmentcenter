e0c0fad57e74f121f5059353429c3bad67a0563f
alexebennett <no.one.likes.the.internet@gmail.com>
2017-07-02 21:53:46 +1000

    Add files via upload

--- media.py
+++ media.py
@@ -1,13 +1,14 @@
 # Import webbrowser module
 import webbrowser
 
-class Movie():
-# Constructor for the movie class to create instances of mMvie.
-  def __init__(self, title, synopsis, poster_image, trailer_youtube, release_date, starring):
-    self.title = title
-    self.synopsis = synopsis
-    self.poster_image_url = poster_image
-    self.trailer_youtube_url = trailer_youtube
-    self.release_date = release_date
-    self.starring = starring
 
+class Movie():
+    # Constructor for the movie class to create instances of Movie.
+    def __init__(self, title, synopsis, poster_image, trailer_youtube,
+                 release_date, starring):
+        self.title = title
+        self.synopsis = synopsis
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+        self.release_date = release_date
+        self.starring = starring
